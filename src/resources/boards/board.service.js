0830e1c43e4b3f13ca9f04df91ecb406ba3df360
gornolis <svln.tarasova@gmail.com>
2020-04-06 19:39:37 +0300

    add board function delete

--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,5 +1,6 @@
 const boardsRepo = require('./board.memory.repository');
 const Board = require('./board.model');
+const tasksService = require('../tasks/task.service');
 
 const getAll = () => boardsRepo.getAll();
 
@@ -16,6 +17,14 @@ const addBoard = params => {
 
 const updateBoard = (id, params) => boardsRepo.updateBoard(id, params);
 
-const deleteBoard = id => boardsRepo.deleteBoard(id);
+const deleteBoard = id => {
+  if (boardsRepo.getBoard(id)) {
+    const tasks = tasksService.getTaskByBoardId(id);
+    for (let i = 0; i < tasks.length; i++) {
+      tasksService.deleteTask(id, tasks[i].id);
+    }
+  }
+  return boardsRepo.deleteBoard(id);
+};
 
 module.exports = { getAll, getBoard, addBoard, updateBoard, deleteBoard };
