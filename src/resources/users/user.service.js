e5674afd388a9c31e5ce26ad706ce85d570e7cde
gornolis <svln.tarasova@gmail.com>
2020-04-06 15:25:46 +0300

    update function delete user

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,6 @@
 const usersRepo = require('./user.memory.repository');
 const User = require('./user.model');
+const tasksService = require('../tasks/task.service');
 
 const getAll = () => usersRepo.getAll();
 
@@ -17,6 +18,14 @@ const addUser = params => {
 
 const updateUser = (id, params) => usersRepo.updateUser(id, params);
 
-const deleteUser = id => usersRepo.deleteUser(id);
+const deleteUser = id => {
+  const tasks = tasksService.getTaskByUserId(id);
+  for (let i = 0; i < tasks.length; i++) {
+    tasks[i].userId = null;
+    tasksService.updateTask(tasks[i].id, tasks[i].boardId, tasks[i]);
+  }
+  const isDeleted = usersRepo.deleteUser(id);
+  return isDeleted;
+};
 
 module.exports = { getAll, getUser, addUser, updateUser, deleteUser };
