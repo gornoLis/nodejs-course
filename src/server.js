465e02643cea830d0f5bdde8b30f61d3cb6d74e6
gornolis <svln.tarasova@gmail.com>
2020-04-17 19:27:27 +0300

    add mongodb connection

--- src/server.js
+++ src/server.js
@@ -1,5 +1,17 @@
-const { PORT } = require('./common/config');
+const mongoose = require('mongoose');
+const { PORT, MONGO_CONNECTION_STRING } = require('./common/config');
 const app = require('./app');
+
+mongoose.connect(MONGO_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log("we're connected!");
+});
 
 app.listen(PORT, () =>
   console.log(`App is running on http://localhost:${PORT}`)
